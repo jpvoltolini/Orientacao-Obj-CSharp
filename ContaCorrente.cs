c229c0c5a06c70b292bc3eb2b8c46aa02923ee44
João Pedro Voltolini <112399200+jpvoltolini@users.noreply.github.com>
2023-10-23 22:50:04 -0300

    Add files via upload

---
+++ ContaCorrente.cs
@@ -0,0 +1,15 @@
+﻿
+class ContaCorrente
+{
+    public string Numero;
+    public decimal Saldo;
+
+public void ImprimirInfo()=>
+Console.WriteLine($"Conta nº {Numero} possui um saldo de: R${Saldo}.\n");
+
+    public void Depositar(decimal valor)
+    {
+        Saldo += valor;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+var conta1 = new ContaCorrente();
+conta1.Numero = "150.102-0";
+conta1.Saldo = 645;
+conta1.ImprimirInfo();
+conta1.Depositar(100);
+conta1.ImprimirInfo();
+
+var conta2  = new ContaCorrente();
+conta2.Numero = "321.654-9";
+conta2.Saldo = 0;
+conta2.ImprimirInfo();
+conta2.Depositar(100);
+conta2.ImprimirInfo();
